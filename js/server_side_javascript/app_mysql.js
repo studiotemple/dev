456d99e7a78aa3ab46b108681155887a65dfad63
Renz <studiotemple81@gmail.com>
2017-03-13 13:08:43 +0900

    db  수업 마지

--- js/server_side_javascript/app_mysql.js
+++ js/server_side_javascript/app_mysql.js
@@ -70,6 +70,83 @@ app.post('/topic/add', (req, res) => {
   //   res.redirect('/topic/'+title);
   // });
 });
+
+app.get(['/topic/:id/edit'], (req, res) => {
+  var sql = 'SELECT id, title FROM topic';
+  conn.query(sql, (err, topics, fields) => {
+    var id = req.params.id;
+    if(id){
+      var sql = 'SELECT * FROM topic WHERE id=?'
+      conn.query(sql, [id], (err, topic, field) => {
+        if(err){
+          console.log(err);
+          res.status(500).send('500 err');
+          //   }
+        } else {
+          res.render('edit', {topics: topics, topic:topic[0]});
+        }
+      });
+    } else {
+      console.log('there is no id');
+      res.status(500).send('500 err');
+      res.render('view', {topics: topics});
+    }
+  });
+});
+
+app.post('/topic/:id/edit', (req, res)=> {
+  var title = req.body.title;
+  var description = req.body.description;
+  var author = req.body.author;
+  var id = req.params.id;
+  var sql = 'UPDATE topic SET title=?, description=?, author=? WHERE id=?';
+  var params = [title, description, author, id];
+  conn.query(sql, params, (err, result, feilds) => {
+    if(err){
+      console.log(err);
+      res.status(500).send('500 err');
+    } else {
+      res.redirect('/topic/'+id);
+    }
+  });
+});
+
+app.get('/topic/:id/delete', (req, res)=> {
+  var sql = 'SELECT id, title FROM topic';
+  var id = req.params.id;
+  conn.query(sql, (err, topics, fields) => {
+    var sql ='SELECT * FROM topic WHERE id=?';
+    conn.query(sql, id, (err, topic) => {
+      if(err){
+        console.log(err);
+        res.status(500).send('500 err');
+      } else {
+        if(topic.length === 0){
+          console.log('there is no record');
+          res.status(500).send('500 err');
+        } else {
+          res.render('delete', {topics:topics, topic:topic[0]});
+        }
+
+      }
+    });
+  });
+
+});
+
+app.post('/topic/:id/delete', (req, res) => {
+  var id = req.params.id;
+  var sql = 'DELETE FROM topic WHERE id=?';
+  conn.query(sql, id, (err, result, feilds) => {
+    if(err){
+      console.log(err);
+      res.status(500).send('500 err');
+    } else {
+      res.redirect('/topic');
+    }
+  });
+});
+
 app.get(['/topic', '/topic/:id'], (req, res) => {
   var sql = 'SELECT id, title FROM topic';
   conn.query(sql, (err, topics, fields) => {
@@ -85,7 +162,6 @@ app.get(['/topic', '/topic/:id'], (req, res) => {
           res.render('view', {topics: topics, topic:topic[0]});
         }
       });
-
     } else {
       res.render('view', {topics: topics});
     }
