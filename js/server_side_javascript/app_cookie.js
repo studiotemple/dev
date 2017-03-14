3df3c95bfa7132c3593c5986f17782a6f92e2219
Renz <studiotemple81@gmail.com>
2017-03-15 00:06:49 +0900

    오늘은 세션에 대해 공부했다.

--- js/server_side_javascript/app_cookie.js
+++ js/server_side_javascript/app_cookie.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 var cookieParser = require('cookie-parser')
-app.use(cookieParser());
+app.use(cookieParser('alsdkjflksdjf!@#523!@#'));
 var products = {
   1: {title: "마우스"},
   2: {title: "키보드"}
@@ -18,19 +18,47 @@ app.get('/products', (req, res) => {
   res.send(`<h1>상품들은?</h1><ul>${output}</ul><a href="/cart">cart</a>`);
 });
 
-app.get(['/cart','/cart/:id'], (req, res) => {
-  res.send('products');
+app.get('/cart/:id', (req, res) => {
+  var id = req.params.id;
+  if(req.signedCookies.cart){
+    var cart = req.signedCookies.cart;
+  } else {
+    var cart = {};
+  }
+  if(!cart[id]){
+    cart[id] = 0;
+  }
+  cart[id] = parseInt(cart[id])+1;
+  res.cookie('cart', cart, {signed: true});
+  res.redirect('/cart');
+});
+
+app.get('/cart', (req, res) => {
+  var cart = req.signedCookies.cart;
+  if(!cart){
+    res.send('장바구니에 담긴 상품이 없습니다.');
+  } else {
+    var output = '';
+    for(var id in cart){
+      output += `<li>${products[id].title} (수량 : ${cart[id]})</li>`;
+    }
+  }
+  res.send(`
+    <h1>장바구니</h1>
+    <ul>${output}</ul>
+    <a href="/products">상품목록으로</a>
+    `);
 });
 
 app.get('/count', (req, res) => {
-  if(req.cookies.count) {
-    var count = parseInt(req.cookies.count);
+  if(req.signedCookies.count) {
+    var count = parseInt(req.signedCookies.count);
   } else {
     var count = 0;
   }
   count = count+1
-  res.cookie('count', count);
-  res.send('count : '+req.cookies.count);
+  res.cookie('count', count, {signed:true});
+  res.send('count : '+ count);
 });
 app.listen(3003, () => {
   console.log('Conneted 3003 port!!');
---
+++ js/server_side_javascript/app_session_mysql.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const session = require('express-session');
+const MySQLStore= require('express-mysql-session')(session);
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(session({
+  secret: 'babara',
+  resave: false,
+  saveUnintialized: true,
+  store: new MySQLStore({
+      host     : 'localhost',
+      port     : 3306,
+      user     : 'root',
+      password : 'nirvana1998',
+      database : 'os'
+  })
+}));
+
+app.get('/count', (req, res) => {
+  if(req.session.count){
+    req.session.count++;
+  } else {
+    req.session.count = 1;
+  }
+  res.send('count : '+req.session.count);
+});
+
+app.get('/auth/login', (req, res) => {
+  var output = `
+  <h1>로그인</h1>
+  <form action="/auth/login" method="post">
+    <p>
+      <input type="text" name="id" placeholder="아이디">
+    </p>
+    <p>
+      <input type="password" name="password" placeholder="비밀번호">
+    </p>
+    <p>
+      <input type="submit">
+    </p>
+  </form>
+  `;
+  res.send(output);
+});
+
+app.post('/auth/login', (req, res) => {
+  var user = {
+    id: 'studiotemple',
+    password: 'nirvana1998',
+    nickname: 'renz'
+  }
+  var id =req.body.id;
+  var pwd = req.body.password;
+  if(id === user.id && pwd === user.password){
+    req.session.nickname = user.nickname;
+    req.session.save(function(){
+    res.redirect('/welcome');
+    });
+  } else {
+    res.send('babo <a href="/auth/login">로그인</a>');
+  }
+});
+
+app.get('/welcome', (req, res) => {
+  if(req.session.nickname){
+    res.send(`
+      <h1>안녕하세요, ${req.session.nickname}님</h1>
+      <a href="/auth/logout">로그아웃</a>
+    `);
+  } else {
+    res.send(`
+      <h1>아이디 또는 비밀번호가 다릅니다.</h1>
+      <a href="/auth/login">로그인</a>
+    `);
+  }
+});
+
+app.get('/auth/logout', (req, res) => {
+  delete req.session.nickname;
+  req.session.save(function(){
+  res.redirect('/welcome');
+  });
+
+});
+
+app.listen(3003, () => {
+  console.log('Conneted 3003 port!!');
+});
